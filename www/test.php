8a4b93d23fe117b1e70e11e24316ab6e51fb6d3c
lauralee01 <grace01>
2017-11-15 13:40:09 -0800

    upload picture files

--- www/test.php
+++ www/test.php
@@ -1,17 +1,8 @@
-<?php
-
-	define('DBNAME', 'classApp');
-	define('DBUSER', 'root');
-	define('DBPASS', 'OLuwaseun01');
-
-   try{
-	$conn = new PDO('mysql:host=localhost;dbname'.DBNAME, DBUSER, DBPASS);
-
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
-
-  } catch(PDOException $err) {
-  		echo $err->getMessage();
+<form id="register" method="POST", enctype="multipart/form-data">
+	<p>Please upload a picture</p>
+	<input type="file" name="pics">
 	
-}
 
-?>
+	<input type="submit" name="save">
+
+</form>
