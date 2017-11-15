ec0024bf520415aedba6fb003abf3ce0cde3fb52
lauralee01 <grace01>
2017-11-15 14:39:28 -0800

    validate the picture file

--- www/test.php
+++ www/test.php
@@ -1,3 +1,37 @@
+<?php
+	
+	define('MAX_FILE_SIZE', '2097152');
+	$ext = ['image/jpg', 'image/jpeg', 'image/png'];
+
+	if(array_key_exists('save', $_POST)) {
+		//print_r($_FILES);
+		$errors = [];
+		if(empty($_FILES['pics']['name'])) {
+			$errors[] = "Please select a file";
+		}
+		if($_FILES['pics']['size'] > MAX_FILE_SIZE) {
+			$errors[] = "File too large. Maximum: ".MAX_FILE_SIZE;
+		}
+		if(!in_array($_FILES['pics']['type'], $ext)) {
+			$errors[] = "File format not supported";
+		}
+		$rnd = rand(0000000000, 9999999999);
+		$strip_name = ste_replace();
+		if(empty($errors)) {
+			echo "File upload successful";
+		} else {
+			foreach ($errors as $err) {
+				echo $err.'</br>';
+			}
+		}
+	}
+
+
+
+?>
+
+
+
 <form id="register" method="POST", enctype="multipart/form-data">
 	<p>Please upload a picture</p>
 	<input type="file" name="pics">
